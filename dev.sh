55e278f078a6ddf4a4221164a477b6c6b59e1dd4
port <109140471+port3000@users.noreply.github.com>
2022-08-05 15:07:42 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -153,7 +153,7 @@ case $n in
   4)
     sleep 1
 
-    echo "$(tput setaf 3)Ok, I will download 1.17.1 and start it for you."
+    echo "$(tput setaf 3)Ok, I will download 1.17.1 and start it for you.."
 
     sleep 4
 
